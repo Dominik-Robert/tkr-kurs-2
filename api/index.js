8c3764d775a00ef58759aee9c7f7185373abeac2
Dominik Robert <dominikrobert@gmx.de>
2021-04-07 22:36:32 +0200

    Datenbank hinzugefügt mit SQL-Datei und der index.js mit den Create Statements und einem Test

---
+++ api/index.js
@@ -0,0 +1,75 @@
+let meinSQLStatement = [
+  `CREATE TABLE IF NOT EXISTS Autor
+(
+  AutorID INT AUTO_INCREMENT,
+  Name TEXT,
+  Vorname text,
+  Username text,
+  Password text,
+  PRIMARY KEY(AutorID)
+);`,
+  `CREATE TABLE IF NOT EXISTS Berechtigungen
+(
+  BerechtigungsID int,
+  Name text,
+  Beschreibung text,
+  PRIMARY KEY(BerechtigungsID)
+);`,
+  `CREATE TABLE IF NOT EXISTS Tags
+(
+  TagName text,
+  PRIMARY KEY(TagName)
+);
+`,
+  `CREATE TABLE IF NOT EXISTS Notiz
+(
+  NotizID int,
+  Inhalt text,
+  Erstellung int,
+  LetzteAenderung int,
+  Titel int,
+  PRIMARY KEY(NotizID)
+);`,
+`CREATE TABLE IF NOT EXISTS NotizTag
+(
+  NotizID int,
+  TagName text,
+  PRIMARY KEY(NotizID, TagName),
+  FOREIGN  KEY(NotizID) REFERENCES Notiz(NotizID),
+  FOREIGN  KEY(TagName) REFERENCES Tags(TagName)
+);`,
+`CREATE TABLE IF NOT EXISTS AutorNotiz
+(
+  AutorID int,
+  NotizID int,
+  BerechtigungID int,
+  PRIMARY KEY(AutorID, NotizID),
+  FOREIGN  KEY(NotizID) REFERENCES Notiz(NotizID),
+  FOREIGN  KEY(AutorID) REFERENCES Autor(AutorID),
+  FOREIGN  KEY(BerechtigungID) REFERENCES Berechtigungen(BerechtigungsID)
+);
+`];
+
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./database.db');
+
+db.serialize(() => {
+  for (query of meinSQLStatement) {
+    db.run(query, (result, err) => {
+      console.log(result, err)
+    });
+  }
+  
+});
+
+
+db.run("INSERT INTO Notiz(Inhalt) VALUES ('Test')");
+
+db.each("SELECT * FROM Notiz", function (err, row) {
+  if (err) {
+    console.log('Error arised', err);
+  }
+  console.log('Mein Inhalt ist', row.Inhalt);
+})
+db.close();
--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+		<header>
+			<div id="logo">Notely</div>
+		</header>
+		<div class="pageContent">
+			<div class="sidebar"></div>
+			<div class="explorerSection">
+				<div class="addbar"></div>
+				<div class="explorer"></div>
+			</div>
+		</div>
+	</body>
   );
 }
 

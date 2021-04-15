b7835f6d3a9671e4e9a89a58ff7126385d839cbd
Dominik Robert <dominikrobert@gmx.de>
2021-04-15 20:43:18 +0200

    Neue Version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 
+/*
 function App() {
   return (
     <body>
@@ -16,6 +17,60 @@ function App() {
 		</div>
 	</body>
   );
+}
+*/
+
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [],
+			loading: true,
+		};
+	}
+
+	componentDidMount() {
+		fetch('http://localhost:3001/api/v1/notes')
+			.then(response => response.json())
+			.then(myData => {
+				console.log('My Data', myData)
+				this.setState({
+					data: myData,
+					loading: false,
+				})
+			}).catch((err) => {
+				console.log(err)
+			})
+		}
+
+	render() {
+		console.log(this);
+		if (this.state.loading) {
+			console.log('IF')
+			return (
+				<div><h1>Loading</h1></div>
+			);
+		} else {
+			console.log('ELSE');
+			return (
+				<div>
+					<header>
+						<div id="logo">Notely</div>
+					</header>
+					<div className="pageContent">
+					<div className="sidebar"></div>
+						<div className="explorerSection">
+							<div className="addbar"></div>
+							<div className="explorer"></div>
+						</div>
+					</div>
+					<p>{ this.state.data[0].Inhalt} </p>
+				</div>
+			);
+		}
+		
+	}
 }
 
 export default App;
